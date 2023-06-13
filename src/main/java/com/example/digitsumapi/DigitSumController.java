d34fe5213fc65ec38110e77b1854e71fd9b88ef3
David Tinhofer <da@w23.at>
2023-06-13 10:06:00 +0200

    created a RestController called DigitSumController, added /api/digitsum feature including tests

---
+++ src/main/java/com/example/digitsumapi/DigitSumController.java
@@ -0,0 +1,22 @@
+package com.example.digitsumapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DigitSumController {
+
+    @GetMapping("/api/digitsum")
+    public int getSum(@RequestParam(name = "number") int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+}
