bc997fb4f318a601a53e0127bf5aba9c9d2e0f63
David Tinhofer <da@w23.at>
2023-06-13 09:45:07 +0200

    Initial commit

---
+++ src/test/java/com/example/digitsumapi/DigitSumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitsumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitSumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
